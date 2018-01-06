6a76c6928feb68a5b46410c5921094a8c173bf7f
Brutalbeard <jceloria@gmail.com>
2018-01-06 12:31:07 -0500

    First push

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source "https://rubygems.org"
+
+gem "lita"
+
+# Uncomment to use the HipChat adapter
+# gem "lita-hipchat"
+gem "lita-discord_oauth"
+
+# Uncomment to use the IRC adapter
+# gem "lita-irc"
+
+# Add handlers to give Lita new functionality.
+# For example:
+# gem "lita-google-images"
+gem "dotenv"
+
+gem "lita-karma"
+gem "lita-abuse"
+
+gem "lita-against-humanity"
+gem "lita-applause"
+gem "lita-ascii-art"
+gem "lita-baby_elephant"
+gem "lita-catfacts", :git => 'https://github.com/Brutalbeard/lita-catfacts.git'
+gem "lita-gifme"
+gem "lita-coin"
+gem "lita-dice"
+gem "lita-eightball"
+gem "lita-urban-dictionary"
---
+++ lita_config.rb
@@ -0,0 +1,42 @@
+Lita.configure do |config|
+
+  Dotenv.load('./.env')
+
+  # The name your robot will use.
+  config.robot.name = "RollToDodge"
+  config.robot.mention_name = "RollToDodge"
+
+  # The locale code for the language to use.
+  config.robot.locale = :en
+  config.robot.alias = "*"
+
+  # The severity of messages to log. Options are:
+  # :debug, :info, :warn, :error, :fatal
+  # Messages at the selected level and above will be logged.
+  config.robot.log_level = :info
+
+  # An array of user IDs that are considered administrators. These users
+  # the ability to add and remove other users from authorization groups.
+  # What is considered a user ID will change depending on which adapter you use.
+  # config.robot.admins = ["1", "2"]
+
+  # The adapter you want to connect with. Make sure you've added the
+  # appropriate gem to the Gemfile.
+  config.robot.adapter = :discord_oauth
+
+  ## Example: Set options for the chosen adapter.
+  # config.adapter.username = "myname"
+  # config.adapter.password = "secret"
+
+  config.adapters.discord_oauth.client = ENV['discord_oauth.client']
+  config.adapters.discord_oauth.token = ENV['discord_oauth.token']
+
+  ## Example: Set options for the Redis connection.
+  # config.redis.host = "127.0.0.1"
+  # config.redis.port = 1234
+
+  ## Example: Set configuration for any loaded handlers. See the handler's
+  ## documentation for options.
+  # config.handlers.some_handler.some_config_key = "value"
+  config.handlers.gifme.api_key = ENV['gifme.api_key']
+end
